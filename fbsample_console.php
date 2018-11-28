5a26f881fb97d55a819d252834b439f936a27618
Frédéric BENOIST <frederic.benoist@free.fr>
2018-11-28 22:07:18 +0100

    first commit

---
+++ fbsample_console.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * 2007-2018 Frédéric BENOIST
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    Frédéric BENOIST <http://www.fbenoist.com/>
+ * @copyright 2013-2018 Frédéric BENOIST <contact@fbenoist.com>
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+
+if (!defined('_PS_VERSION_')) {
+     exit;
+}
+
+class FbSample_Console extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'fbsample_console';
+        $this->version = '1.7.0';
+        $this->author = 'Frederic BENOIST';
+        $this->tab = 'others';
+        $this->bootstrap = true;
+        $this->ps_versions_compliancy = array(
+            'min' => '1.7.5',
+            'max' => _PS_VERSION_
+        );
+        parent::__construct();
+        $this->displayName = $this->l('Sample console command Module ');       
+        $this->description = $this->l('Add console command with PrestaShop 1.7 module.');
+    }
+}
---
+++ src/Command/MyCommand.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * 2007-2018 Frédéric BENOIST
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    Frédéric BENOIST <http://www.fbenoist.com/>
+ * @copyright 2013-2018 Frédéric BENOIST <contact@fbenoist.com>
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+
+namespace Fbenoist\FbSample_Console\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Sample MyCommand
+ */
+class MyCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            // the name of the command (the part after "bin/console")
+            ->setName('fbsample_console:mycommand')
+
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Run sample command in PrestaShop module.')
+
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp('This command allows you to run command in PrestaShop module.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->write('In fbsample_console, MyCommand is running !');
+    }
+}
